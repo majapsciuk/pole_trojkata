6520e6172a3e1f6dcd91d007c9620db38193f9a2
majapsciuk <37487029+majapsciuk@users.noreply.github.com>
2018-03-17 22:26:24 +0100

    Create pole.py

---
+++ pole.py
@@ -0,0 +1,5 @@
+print("czesc tu Maja. Program wyliczajacy pole trojkata")
+a = input("podaj a ");
+h = input("podaj h "); 
+pole = a*h/2;
+print "Pole trojkata o podstawie:", a, "i wysokosci:", h, "to:", pole;
